2a9b84f20b97fd16fa261ae5377af910adbd7dd1
Brian-Weloba <bweloba@gmail.com>
2022-03-24 17:10:23 +0300

    Add javadoc

--- src/main/java/me/bweloba/gameoflife/Main.java
+++ src/main/java/me/bweloba/gameoflife/Main.java
@@ -9,28 +9,51 @@ import me.bweloba.gameoflife.components.SplitPaneContainer;
 
 import java.io.IOException;
 
+/**
+ * <p>
+ *     Main class of the application.
+ *     This class is responsible for initializing the application.
+ *     </p>
+ * @author Brian Weloba
+ * @version 1.0
+ * @since 1.0
+ * @see Application
+ * @see Stage
+ * @see Scene
+ */
 public class Main extends Application {
 
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         launch();
     }
 
+    /**
+     *  The start() method is called after the init() method has returned.
+     * @param stage the primary stage for this application
+     * @throws IOException if the fxml file could not be loaded
+     */
     @Override
     public void start(Stage stage) throws IOException {
-
+        // Make the stage full screen
         Screen screen = Screen.getPrimary();
         Rectangle2D bounds = screen.getVisualBounds();
-
         stage.setX(bounds.getMinX());
         stage.setY(bounds.getMinY());
         stage.setWidth(bounds.getWidth());
         stage.setHeight(bounds.getHeight());
+        //Disable resizing
         stage.setResizable(false);
+        //Set the title
         stage.setTitle("GAME OF LIFE");
-
+        //Add the split pane container to the scene
         Scene scene = new Scene(SplitPaneContainer.addSplitPane());
+        //set the scene
         stage.setScene(scene);
-
+        //show the stage
         stage.show();
     }
 }
--- src/main/java/me/bweloba/gameoflife/components/SplitPaneContainer.java
+++ src/main/java/me/bweloba/gameoflife/components/SplitPaneContainer.java
@@ -1,11 +1,27 @@
 package me.bweloba.gameoflife.components;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.SplitPane;
 import me.bweloba.gameoflife.Main;
 
 import java.io.IOException;
 
+/**
+ * @author Brian Weloba
+ * @version 1.0
+ * <p>
+ * This class is used to load the FXML file for the SplitPane.
+ * </p>
+ * @see SplitPane
+ * @since 1.0
+ */
 public class SplitPaneContainer {
+    /**
+     * This method is used to load the FXML file for the SplitPane.
+     *
+     * @return SplitPane object
+     * @throws IOException if the FXML file cannot be loaded
+     */
     public static javafx.scene.control.SplitPane addSplitPane() throws IOException {
         javafx.scene.control.SplitPane splitPane = new javafx.scene.control.SplitPane();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("menu-view.fxml"));
